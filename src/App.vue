de4ffc16c3450f5b8b78328988b57b44dd8f4c21
st3v3f <st3v3f@gmail.com>
2019-01-05 19:58:54 +0000

    Adding buefy (bulma) and test components.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <Navbar/>
+
+<section>
+  <div class="columns">
+    <div class="column">
+      First column
+      <img alt="Vue logo" src="./assets/logo.png">
+    </div>
+    <div class="column">
+      Second column
+      <HelloTest/>
+    </div>
+    <div class="column">
+      Third column
+      <DatePicker/>
+    </div>
+    <div class="column">
+      Fourth column
+    </div>
+  </div>
+</section>
+
+  <Page1/>
+  
+  <Page2/>
+    
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import DatePicker from './components/DatePicker.vue'
+import HelloTest from './components/HelloTest.vue'
+import Navbar from './components/Navbar.vue'
+import Page1 from './components/Page1.vue'
+import Page2 from './components/Page2.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Navbar,
+    HelloTest,
+    DatePicker,
+    Page1,
+    Page2
   }
 }
 </script>
@@ -21,8 +56,10 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  // text-align: center;
+  // color: #2c3e50;
+   margin-top: 20px;
+   margin-left: 200px;
+   margin-right: 200px;
 }
 </style>
